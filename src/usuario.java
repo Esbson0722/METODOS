2e0ccef1bce16565a2c008bf0842a171cecfaa35
binho <esbsonalmeida@gmail.com>
2024-06-14 01:21:49 -0300

    SmatTv

--- src/usuario.java
+++ src/usuario.java
@@ -1,4 +1,4 @@
-public class App {
+public class usuario {
     public static void main(String[] args) throws Exception {
         
         SmartTv smartTv = new SmartTv();
@@ -7,13 +7,21 @@ public class App {
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.diminuirVolume();
+        System.out.println("Volume atual : " + smartTv.volume);
 
         System.out.println("Canal Atual : " + smartTv.canal);
 
         smartTv.mudarCanal(13);
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.diminuirCanal();
+        smartTv.diminuirCanal();
         System.out.println("Canal Atual : " + smartTv.canal);
 
-        System.out.println("Volume atual : " + smartTv.volume);
+        
 
         System.out.println("Tv Ligada ? " + smartTv.ligada);
         System.out.println("Canal Atual : " + smartTv.canal);
